1108a0b29b70bc05643b884c42effddb2acee237
Alexandrrrrrrrrr <alexander_radics.stu@willowschoolnola.org>
2023-08-31 13:42:57 -0500

    Create HelloWorldAlexanderR.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorldAlexanderR.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+public class HelloWorldAlexanderR extends OpMode {
+    @Override
+    public void init() {
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+        telemetry.addData("Hello","World");
+    }
+    @Override
+    public void loop() {
+
+    }
+}
