fc7b7fb41f844249d42a884f0ee5741a59d77c37
Alexandrrrrrrrrr <143218456+Alexandrrrrrrrrr@users.noreply.github.com>
2023-09-12 12:20:16 -0500

    Update Gamepad_Testing_AR.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad_Testing_AR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad_Testing_AR.java
@@ -2,11 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 
 @TeleOp()
 public class Gamepad_Testing_AR extends OpMode {
     @Override
     public void init() {
+      FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
     }
 
     @Override
