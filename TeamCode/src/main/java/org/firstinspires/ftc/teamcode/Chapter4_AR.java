ce0c5697cb3fac1cb74c4530b2989e7b26cd348d
Alexandrrrrrrrrr <143218456+Alexandrrrrrrrrr@users.noreply.github.com>
2023-09-19 12:37:07 -0500

    Update Chapter4_AR.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chapter4_AR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chapter4_AR.java
@@ -1,33 +1,35 @@
-package org.firstinspires.ftc.teamcode;
-
+package org.firstinspires.ftc.teamcode.solutions;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp()
+@TeleOp
 public class Chapter4_AR extends OpMode {
-
     @Override
     public void init() {
     }
+
     @Override
     public void loop() {
-        double speedForward = -gamepad1.left_stick_y / 2.0;
-        telemetry.addData("Left stick y", gamepad1.left_stick_y);
-        telemetry.addData("speed Forward", speedForward);
-        if (gamepad1.a) {
-            speedForward = 1;
-            }
-        else if (!gamepad1.a) {
-            speedForward = 0.5;
+        double fwdSpeed = gamepad1.left_stick_y;
+
+        if (!gamepad1.a) // turbo mode
+        {
+            fwdSpeed *= 0.5;
         }
-while (gamepad1.a) {
-    telemetry.addData(gamepad1.left_stick_y = gamepad1.left_stick_x);
-    telemetry.addData(gamepad1.left_stick_x = gamepad1.right_stick_y);
-}
-while (!gamepad1.a) {
-    telemetry.addData(gamepad1.left_stick_y = gamepad1.left_stick_y);
-    telemetry.addData(gamepad1.left_stick_x = gamepad1.left_stick_x);
-}
+
+        telemetry.addData("Forward Speed", fwdSpeed);
+        double ySpeed = gamepad1.left_stick_y;
+        double xSpeed = gamepad1.left_stick_x;
+
+        if (gamepad1.a) { // crazy mode
+            ySpeed = gamepad1.left_stick_x;
+            xSpeed = gamepad1.left_stick_y;
         }
+
+        telemetry.addData("Y Speed", ySpeed);
+        telemetry.addData("X Speed", xSpeed);
+
     }
+}
+
